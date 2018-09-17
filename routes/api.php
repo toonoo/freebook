c3676eb6c726b251bfeea6d3638d5b227a6cfd2a
chatchai <root@great-mac.local>
2018-09-17 11:20:39 +0700

    retrun to working hard

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware('auth:api')->get('/book', function (Request $request) {
-    return $request->user();
-});
-
-Route::middleware('auth:api')->get('/books', function (Request $request) {
-    return $request->user();
-});
-
-Route::middleware('auth:api')->get('/bookss', function (Request $request) {
-    return $request->user();
-});
